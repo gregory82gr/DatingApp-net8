b0d85f123639e83a6a5cddde72e836f2aca8955c
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2024-12-28 12:50:42 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddDbContext<DataContext>(opt=>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +25,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 
