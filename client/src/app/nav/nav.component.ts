4b61e97cc1ebb27e8e9dc9548780d8dfe966ee9d
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-03-01 17:12:44 +0200

    nav && service

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+import { AccountService } from '../_services/account.service';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+@Component({
+  selector: 'app-nav',
+  imports: [FormsModule,BsDropdownModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+  private accountService=inject(AccountService);
+  loggedIn=false;
+  model:any={};
+
+  login(){
+    this.accountService.login(this.model).subscribe(response=>{
+      console.log(response);
+      this.loggedIn=true;
+    },error=>{
+      console.log(error);
+    });
+    console.log(this.model);
+  }
+
+  logout(){
+    this.loggedIn=false;
+  }
+}
