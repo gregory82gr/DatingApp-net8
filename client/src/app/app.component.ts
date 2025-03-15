5eec7154c0b3eceb3a560d627a4faba9bd515843
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-03-15 16:55:07 +0200

    end of section 55

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,26 +1,24 @@
 import { NgFor } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AccountService } from './_services/account.service';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet,NgFor,NavComponent],
+  imports: [RouterOutlet,NgFor,NavComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent  implements OnInit{
- http=inject(HttpClient);
+
  private accountService = inject(AccountService);
 
   title = 'client';
-  users:any;
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -30,11 +28,5 @@ export class AppComponent  implements OnInit{
     const user=JSON.parse(userString);
     this.accountService.currentUser.set(user);
   }
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users=response,
-      error: error=>console.log(error),
-      complete: () => console.log('Request has completed')
-    })
-  }
+
 }
