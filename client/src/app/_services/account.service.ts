c7935b704a191937a200aa687f48a3c7188e171a
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-03-15 22:54:09 +0200

    End of Section 5 (57)

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -24,6 +24,19 @@ export class AccountService {
     )
   }
 
+  register(model:any){
+    return this.http.post<User>(this.baseUrl+'account/register',model).pipe(
+      map((response:User)=>{
+        const user=response;
+        if(user){
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+        return user;
+      })
+    )
+  }
+
   logout(){
     localStorage.removeItem('user');
     this.currentUser.set(null);
