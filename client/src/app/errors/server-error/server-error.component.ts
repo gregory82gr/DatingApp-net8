11822073bc52c51bf7c7669298d6c4c67c8a68c5
gregory82gr <gregory82gr@yahoo.gr>
2025-03-25 13:46:41 +0200

    End of section 7

--- client/src/app/errors/server-error/server-error.component.ts
+++ client/src/app/errors/server-error/server-error.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-server-error',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './server-error.component.css'
 })
 export class ServerErrorComponent {
+  error:any;
 
+  constructor(private router:Router){
+    const navigation=this.router.getCurrentNavigation();
+    this.error=navigation?.extras?.state?.['error'];
+  }
+  
 }
