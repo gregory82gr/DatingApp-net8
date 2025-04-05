1bfbc6a20c1049a9e89fedbc71b20f5c0af4c744
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-04-06 01:20:22 +0300

    end of section 87

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,24 +7,25 @@ using Microsoft.AspNetCore.Authorization;
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(DataContext context) : BaseApiController
+public class UsersController(IUserRepository userRepository) : BaseApiController
 {
-  [AllowAnonymous]
+  
   [HttpGet]
   public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
   {
-        var users= await context.Users.ToListAsync();
+        var users= await userRepository.GetUsersAsync();
 
-        return users;
+        return Ok(users);
   }
 
-    [Authorize]
-    [HttpGet("{id}")]
-  public async  Task<ActionResult<AppUser>> GetUser(int id)
+    
+    [HttpGet("{username}")]
+  public async  Task<ActionResult<AppUser>> GetUser(string username)
   {
-        var user= await context.Users.FindAsync(id);
+        var user= await userRepository.GetUserByUsernameAsync(username);
 
         if(user==null) return NotFound();
         return user;
   }
+
 }
---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,38 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using API.Interfaces;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace API.Data;
+
+
+public class UserRepository(DataContext _context) : IUserRepository
+{
+    public void Update(AppUser user)
+    {
+        _context.Entry(user).State = EntityState.Modified;
+    }
+
+    public async Task<bool> SaveAllAsync()
+    {
+        return await _context.SaveChangesAsync() > 0;
+    }
+
+    public async Task<AppUser?> GetUserByIdAsync(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+    public async Task<AppUser?> GetUserByUsernameAsync(string username)
+    {
+        return await _context.Users.SingleOrDefaultAsync(x => x.UserName == username.ToLower());
+    }
+
+    public async Task<IEnumerable<AppUser>> GetUsersAsync()
+    {
+        return await _context.Users
+        .Include(x=>x.Photos).
+        ToListAsync();
+    }
+}
