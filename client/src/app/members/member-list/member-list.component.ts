3a2d73acb6b25446c75900b78f788d3b303d674a
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-04-07 18:16:40 +0300

    end of section 98

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { Member } from '../../_models/member';
 
 @Component({
   selector: 'app-member-list',
@@ -6,6 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
-export class MemberListComponent {
+export class MemberListComponent  implements OnInit {
+
+  private memberService=inject(MembersService);
+  members: Member[] = [];
+
+
+
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe(members => {
+      this.members = members;
+    }, error => {
+      console.error(error);
+    });
+  }
 
 }
