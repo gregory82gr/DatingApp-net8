e23bac34f676bbe385bd43d9d28c1824451ee0c6
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-04-07 17:19:16 +0300

    end of section 96

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -5,14 +5,14 @@ using API.Extensions;
 
 namespace API.Helpers
 {
-   
+
 
     public class AutoMapperProfiles : Profile
     {
         public AutoMapperProfiles()
         {
             CreateMap<AppUser, MemberDto>()
-            .ForMember(dest=>dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()))
                 .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain)!.Url));
             CreateMap<Photo, PhotoDto>();
         }
