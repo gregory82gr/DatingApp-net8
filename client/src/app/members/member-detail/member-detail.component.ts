b129181796047b83c5182108aa13d799679c99af
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-04-10 12:17:42 +0300

    end of section 9

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -2,10 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import {  GalleryItem, GalleryModule, ImageItem ,Gallery} from 'ng-gallery';
 
 @Component({
   selector: 'app-member-detail',
-  imports: [],
+  imports: [TabsModule,GalleryModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
@@ -13,10 +15,16 @@ export class MemberDetailComponent implements OnInit {
  private memberService=inject(MembersService);
  private route=inject(ActivatedRoute);
  member?: Member ;
+ images:GalleryItem[]=[];
 
+ constructor(private gallery: Gallery) {}
   ngOnInit(): void {
     this.loadMember();
+    console.log('Gallery images:', this.images);
+
   }
+
+
 
 
 
@@ -25,6 +33,9 @@ export class MemberDetailComponent implements OnInit {
     if (username) {
       this.memberService.getMember(username).subscribe(member => {
         this.member = member;
+        member.photos.map(p=>{
+          this.images.push(new ImageItem({src:p.url, thumb:p.url}))
+        })
       }, error => {
         console.error(error);
       });
