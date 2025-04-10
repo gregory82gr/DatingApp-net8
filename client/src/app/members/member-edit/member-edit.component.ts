c5636a486022147a9ba929fb52bf0215f5542f05
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-04-10 16:56:52 +0300

    end of section 109

---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,38 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Member } from '../../_models/member';
+import { MembersService } from '../../_services/members.service';
+import { AccountService } from '../../_services/account.service';
+
+@Component({
+  selector: 'app-member-edit',
+  imports: [],
+  templateUrl: './member-edit.component.html',
+  styleUrl: './member-edit.component.css'
+})
+export class MemberEditComponent implements OnInit {
+  member?: Member;
+  private memberService = inject(MembersService);
+  private accountService = inject(AccountService);
+
+
+  ngOnInit(): void {
+    this.loadMember();
+
+  }
+
+  loadMember() {
+      const user =this.accountService.currentUser();
+      if(!user) return;
+      console.log('Member:', user);
+      if (user.userName) {
+        this.memberService.getMember(user.userName).subscribe(member => {
+          this.member = member;
+          console.log('Member:', member);
+        }, error => {
+          console.error(error);
+        });
+      }
+  }
+
+
+}
