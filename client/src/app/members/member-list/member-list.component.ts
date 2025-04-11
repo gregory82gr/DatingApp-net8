5ea434ccf9e7014765c52eea77af9840fd8e945e
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-04-11 14:46:18 +0300

    End of section 10

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -11,21 +11,19 @@ import { MemberCardComponent } from '../member-card/member-card.component';
 })
 export class MemberListComponent  implements OnInit {
 
-  private memberService=inject(MembersService);
+  memberService=inject(MembersService);
   members: Member[] = [];
 
 
 
   ngOnInit(): void {
+    if(this.memberService.members().length === 0) {
     this.loadMembers();
+    }
   }
 
   loadMembers() {
-    this.memberService.getMembers().subscribe(members => {
-      this.members = members;
-    }, error => {
-      console.error(error);
-    });
+    this.memberService.getMembers();
   }
 
 }
