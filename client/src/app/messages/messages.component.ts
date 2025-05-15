464761024e2d3cd4a2cb031eb4682861c214a1c7
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-05-15 15:05:13 +0300

    end of section 185

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,9 +1,15 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MessageService } from '../_services/message.service';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule } from '@angular/forms';
+import { TimeagoModule } from 'ngx-timeago';
+import { Message } from '../_models/message';
+import { RouterLink } from '@angular/router';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-messages',
-  imports: [],
+  imports: [ButtonsModule,FormsModule,TimeagoModule,RouterLink,PaginationModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
@@ -23,6 +29,13 @@ export class MessagesComponent  implements OnInit{
 
   }
 
+  getRoute(message: Message){
+    if(this.container==='Outbox'){
+      return '/members/${message.recipientUsername}';
+    }else{
+      return '/members/${message.senderUsername}';
+    }
+  }
   pageChanged(event: any) {
     if (this.pageNumber !== event.page) {
       this.pageNumber = event.page;
